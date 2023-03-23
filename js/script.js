36999afb54e99d60c51a00afca337d4c84b682bf
yaredghebre <yared2391@gmail.com>
2023-03-23 13:53:20 -0300

    Add: Project complete (Bonus1.2)

--- js/script.js
+++ js/script.js
@@ -18,16 +18,46 @@
 // BONUS 3:
 // Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, 
+    {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, 
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, 
+    {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, 
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
-const arrayImages = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
 const itemsContainer = document.querySelector (".slider-items");
 
 
-for (let i = 0;  i < arrayImages.length; i++) {
-    const currentImage = arrayImages [i];      
-    console.log(currentImage);
+for (let i = 0;  i < images.length; i++) {
+    const currentSlide = images [i];      
+    console.log(currentSlide);
 // Concateno con un template literal assegnato a una variabile
-    const sliderItems = `<div class="item"> <img src="${currentImage}"> </div>`;
+    const sliderItems = `<div class="item"> 
+                            <img src="${currentSlide.image}"> 
+                            <h3 class="card-title">${currentSlide.title} </h3>
+                            <p class="card-text">${currentSlide.text} </p>
+                        </div>`;
     itemsContainer.innerHTML += sliderItems;
 }
 
@@ -79,14 +109,12 @@ prevBtn.addEventListener("click", function () {
     setInterval(autoPlay, 3000);    
 });
 
-// COMANDI DI START E STOP ALL'HOVER
-document.querySelector(".slider-items").addEventListener("mouseover", stopCarousel);
-document.querySelector(".slider-items").addEventListener("mouseout", restartCarousel);
-
+// COMANDI DI START E STOP Autoplay
+document.querySelector(".slider-items").addEventListener("click", stopCarousel);
+document.querySelector(".slider-items").addEventListener("click", restartCarousel);
 
 
 // FUNZIONI TIMING
-
 let interval = setInterval(autoPlay, 3000)
 
 function autoPlay() {
@@ -103,7 +131,6 @@ function autoPlay() {
     // una volta passato alla slide successivo, gli assegno classe "active"
     arrayItems[activeItemIndex].classList.add("active");
     console.log(arrayItems);
-
 
 console.log(arrayItems[activeItemIndex]);
 }
